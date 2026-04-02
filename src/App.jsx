87f9db74ac1e57736f44c365f2181f53ad4f69d2
me@DESKTOP-C8BNVUF <godwinapochi@gmail.com>
2026-04-02 14:54:21 -0700

    transition fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import {BrowserRouter,Routes,Route} from "react-router-dom"
+import {motion,AnimatePresence} from "framer-motion"
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
@@ -6,14 +7,56 @@ import Navbar from "./Components/Navbar";
 function App() {
   return (
     <BrowserRouter>
-      <Navbar/>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+        <Navbar />
+        <AnimatedRoutes />
     </BrowserRouter>
   );
 }
+function AnimatedRoutes() {
+  const location = useLocation();
+  return (
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route
+          path="/"
+          element={
+            <PageTransition>
+              <Home />
+            </PageTransition>
+          }
+        />
+        <Route
+          path="/about"
+          element={
+            <PageTransition>
+              <About />
+            </PageTransition>
+          }
+        />
+        <Route
+          path="/contact"
+          element={
+            <PageTransition>
+              <Contact />
+            </PageTransition>
+          }
+        />
+      </Routes>
+    </AnimatePresence>
+  );
+}
 
+function PageTransition({ children }) {
+  return (
+    <motion.main
+      className="page"
+      initial={{ opacity: 0 ,x:50}}
+      animate={{ opacity: 1 ,x:0}}
+      exit={{ opacity: 0 ,x:-50}}
+      transition={{ duration: 0.35,ease:"easeOut" }}
+    >
+      {children}
+    </motion.main>
+  );
+}
 export default App;
